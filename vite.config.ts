1cd0ae4daa8cbc4e495e11b6cf22cb7181d15a92
Dasha <dasha2312s3@gmail.com>
2025-05-02 23:18:37 +0300

    added autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import autoprefixer from 'autoprefixer';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -16,6 +17,10 @@ export default defineConfig({
     }
   },
   css: {
-    postcss: './postcss.config.js',
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
   }
 })
